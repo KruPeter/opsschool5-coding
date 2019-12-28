29e85c461c0bb918fb7670a05898c6773f51cf00
cheisr <54358302+cheisr@users.noreply.github.com>
2019-12-29 01:18:41 +0200

    Update chen_weather_assignment2.py

--- chen_weather_assignment2.py
+++ chen_weather_assignment2.py
@@ -3,27 +3,29 @@ import sys
 import urllib.parse
 from urllib.request import urlretrieve
 
-city = sys.argv[1].split(",")
-city = urllib.parse.quote(str(city))
+cities = sys.argv[1].split(",")
 url_basis = "http://api.weatherstack.com/current?access_key=a9b3b3c03662da4b3e5f21706695d616&query="
+
 if len(sys.argv) < 3 or sys.argv[2] == "-c":
-    url_query = (url_basis + str(city))
     temp_unit = "Celsius"
+    units = ""
 
 elif sys.argv[2] == "-f":
-    url_query = (url_basis + city + "&units=f")
     temp_unit = "Fahrenheit"
+    units = "&units=f"
 
-with urllib.request.urlopen(url_query) as url:
-    data = json.loads(url.read().decode())
+for city in cities:
 
-try:
-    dict_location = data['location']
-    dict_location_city = dict_location.get('name', {})
-    dict_temp = (data['current']).get('temperature', {})
-    print("The weather in " + dict_location_city.capitalize() + " today is " + str(dict_temp) + " degrees " + temp_unit + ".")
-except KeyError:
-    print("The city you're looking for does not exist in our records, how about searching for Milan, Berlin or Dublin instead?")
+    city = urllib.parse.quote(str(city))
+    url_query = (url_basis + city + units)
 
+    with urllib.request.urlopen(url_query) as url:
+        data = json.loads(url.read().decode())
 
-
+    try:
+        dict_location = data['location']
+        dict_location_city = dict_location.get('name', {})
+        dict_temp = (data['current']).get('temperature', {})
+        print("The weather in " + dict_location_city.capitalize() + " today is " + str(dict_temp) + " degrees " + temp_unit + ".")
+    except KeyError:
+        print("The city you're looking for does not exist in our records, how about searching for Milan, Berlin or Dublin instead?")
