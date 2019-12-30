e79dc87b9aa287f334ef6a80d6dafcd610949d0c
cheisr <54358302+cheisr@users.noreply.github.com>
2019-12-30 02:04:22 +0200

    Update chen_weather_assignment2.py
    Per code review, I added error handling so the current version supports the following requests:
    1. `python my_weather.py dublin`
    2. `python my_weather.py dublin -c`
    3. `python my_weather.py dublin -f`
    4. `python my_weather.py lalallalalalallallalal`
    5. `python my_weather.py dublin,rome,milan`
    6. `python my_weather.py`
    7. `python my_weather.py dublin -d`

--- chen_weather_assignment2.py
+++ chen_weather_assignment2.py
@@ -3,21 +3,31 @@ import sys
 import urllib.parse
 from urllib.request import urlretrieve
 
-cities = sys.argv[1].split(",")
-url_basis = "http://api.weatherstack.com/current?access_key=a9b3b3c03662da4b3e5f21706695d616&query="
+access_key = "a9b3b3c03662da4b3e5f21706695d616"
+url_basis = "http://api.weatherstack.com/current?access_key="
+
+if len(sys.argv) > 1:
+    cities = sys.argv[1].split(",")
+else:
+    cities = ""
+    print("Oops! no city was requested, please add a city to your request and try again!")
 
 if len(sys.argv) < 3 or sys.argv[2] == "-c":
-    temp_unit = "Celsius"
+    temperature_unit = "Celsius"
     units = ""
 
 elif sys.argv[2] == "-f":
-    temp_unit = "Fahrenheit"
+    temperature_unit = "Fahrenheit"
     units = "&units=f"
 
-for city in cities:
+elif sys.argv[2] != "-f" or "-c":
+    print("Tip: add '-f' or '-c' to search for the temperature in Fahrenheit or Celsius degrees, respectively")
+    temperature_unit = "Celsius"
+    units = ""
 
+for city in cities:
     city = urllib.parse.quote(str(city))
-    url_query = (url_basis + city + units)
+    url_query = (url_basis + access_key + "&query=" + city + units)
 
     with urllib.request.urlopen(url_query) as url:
         data = json.loads(url.read().decode())
@@ -25,7 +35,7 @@ for city in cities:
     try:
         dict_location = data['location']
         dict_location_city = dict_location.get('name', {})
-        dict_temp = (data['current']).get('temperature', {})
-        print("The weather in " + dict_location_city.capitalize() + " today is " + str(dict_temp) + " degrees " + temp_unit + ".")
+        dict_temperature = (data['current']).get('temperature', {})
+        print("The weather in " + dict_location_city.capitalize() + " today is " + str(dict_temperature) + " degrees " + temperature_unit + ".")
     except KeyError:
         print("The city you're looking for does not exist in our records, how about searching for Milan, Berlin or Dublin instead?")
